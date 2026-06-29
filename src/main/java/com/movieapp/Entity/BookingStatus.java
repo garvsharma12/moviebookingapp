a49d71698b0e813c8bb3946826d69b381d0c1b92
grv <garv.sharma200212@gmail.com>
2026-06-29 22:37:59 +0530

    first commit

---
+++ src/main/java/com/movieapp/Entity/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.movieapp.Entity;
+
+public enum BookingStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
---
+++ src/main/java/com/movieapp/Entity/Theater.java
@@ -0,0 +1,10 @@
+package com.movieapp.Entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class Theater {
+    
+}
