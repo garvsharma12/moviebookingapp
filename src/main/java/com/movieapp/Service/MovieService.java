501a7c43314d2d29e0b7ba81332ba454d0da35e5
grv <garv.sharma200212@gmail.com>
2026-07-06 22:46:03 +0530

    added fuctionality of addMovie()

--- src/main/java/com/movieapp/Service/MovieService.java
+++ src/main/java/com/movieapp/Service/MovieService.java
@@ -1,16 +1,27 @@
 package com.movieapp.Service;
 
 import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.movieapp.DTO.MovieDTO;
+import com.movieapp.Entity.Movie;
+import com.movieapp.Repository.MovieRepository;
 
 @Service
 public class MovieService {
 
-    public @Nullable Object addMovie(MovieDTO movieDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addMovie'");
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public Movie addMovie(MovieDTO movieDTO){
+        Movie movie = new Movie();
+        movie.setTitle(movieDTO.getTitle());
+        movie.setGenre(movieDTO.getGenre());
+        movie.setLanguage(movieDTO.getLanguage());
+        movie.setReleaseDate(movieDTO.getReleaseDate());
+
+        return movieRepository.save(movie);
     }
     
 }
